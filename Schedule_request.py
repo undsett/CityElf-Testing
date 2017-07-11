09205644ef056117d8a1be7ca5a004aefe8506cc
rozhkoandrew <rozhkoandrew@gmail.com>
2017-07-11 10:45:20 +0300

    start parser in regular frequency with sending email system

--- Schedule_request.py
+++ Schedule_request.py
@@ -1,44 +1,38 @@
 import schedule
 import time
 import requests
-import unittest
+import smtplib
+
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+
+    server.login("cityelfparsses@gmail.com", 'qwedsazxccdewsxzaq')
+
+    msg = "Somethink wrong with PARSES work. Please check it!"
+    server.sendmail("cityelfparsses@gmail", "cityelfodessa@gmail.com", msg)
+    server.quit()
 
 def job():
     response = requests.get('http://localhost:8088/services/forecasts/startCollector')
     print("I'm working...", time.ctime(time.time()))
     #print(response.content) #uncomment in case of error to see error message
     print(response)
+    if  str(response) != '<Response [200]>':
+        send_email()
+        print("Somethink wrong, email already send")
     return response
-
-class TestResponse(unittest.TestCase):
-    def test_response(self):
-        str_r = str(start)
-        self.assertTrue(str_r == '<Response [200]>')
 
 
 if __name__ == '__main__':
-    start = schedule.every(6).minutes.do(job)
+    start = schedule.every(7).minutes.do(job)
+
     while 1:
         schedule.run_pending()
-        time.sleep(7)
+        time.sleep(1)
 
 
 
 
-'''import schedule
-import time
-import requests
 
-def job():
-    response = requests.get('http://localhost:8088/services/forecasts/startCollector')
-    print("I'm working...", time.ctime(time.time()))
-    print(response.content) #uncomment in case of error to see error message
-    print(response)
-    return response
 
-schedule.every(1).minutes.do(job)
-
-if __name__ == '__main__':
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)'''
